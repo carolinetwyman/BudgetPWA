9896e3a99d7fc28a296b0b288b06a64eb22858a1
Caroline Twyman <carolinetwyman@gmail.com>
2020-11-10 08:49:33 -0600

    fixing serviceworker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,38 +1,37 @@
 console.log("this is a service worker");
-const FILES_TO_CACHE = [
+const CACHE_FILES = [
     "/",
     "/index.html",
-    "/styles.css",
     "/index.js",
     "/manifest.webmanifest",
+    "/styles.css",
     "/favicon.ico",
     "/icons/icon-512x512.png",
     "/icons/icon-192x192.png",
 
   ];
 
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
+  const CACHE = "static-cache-v2";
+  const DATA_CACHE = "data-cache-v1";
 
-  self.addEventListener("install", function (evt) {
-    evt.waitUntil(
-      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+  self.addEventListener("install", function (e) {
+    e.waitUntil(
+      caches.open(CACHE).then((cache) => cache.add("/api/transaction"))
     );
 
-    evt.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    e.waitUntil(
+      caches.open(CACHE).then((cache) => cache.addAll(CACHE_FILES))
     );
 
     self.skipWaiting();
   });
 
-  self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
-      caches.keys().then(keyList => {
+  self.addEventListener("activate", function(e) {
+    e.waitUntil(
+      caches.keys().then(keys => {
         return Promise.all(
-          keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
+          keys.map(key => {
+            if (key !== CACHE && key !== DATA_CACHE) {
               return caches.delete(key);
             }
           })
@@ -43,20 +42,20 @@ const FILES_TO_CACHE = [
     self.clients.claim();
   });
 
-  self.addEventListener("fetch", function(evt) {
-    if (evt.request.url.includes("/api/")) {
-      evt.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
-          return fetch(evt.request)
+  self.addEventListener("fetch", function(e) {
+    if (e.request.url.includes("/api/")) {
+      e.respondWith(
+        caches.open(DATA_CACHE).then(cache => {
+          return fetch(e.request)
             .then(response => {
               if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
+                cache.put(e.request.url, response.clone());
               }
 
               return response;
             })
             .catch(err => {
-              return cache.match(evt.request);
+              return cache.match(e.request);
             });
         }).catch(err => console.log(err))
       );
@@ -64,10 +63,10 @@ const FILES_TO_CACHE = [
       return;
     }
 
-    evt.respondWith(
-      caches.open(CACHE_NAME).then(cache => {
-        return cache.match(evt.request).then(response => {
-          return response || fetch(evt.request);
+    e.respondWith(
+      caches.open(CACHE).then(cache => {
+        return cache.match(e.request).then(response => {
+          return response || fetch(e.request);
         });
       })
     );
